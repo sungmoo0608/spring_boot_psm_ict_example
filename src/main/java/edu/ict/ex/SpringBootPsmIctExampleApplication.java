a49b1fc26caec08229493be73b6115663fc44cd7
WIN-3QL0R8Q3DTD\ICT02_03 <swingsm0608@gmail.com>
2024-09-11 11:46:36 +0900

    스프링 기본 설정

---
+++ src/main/java/edu/ict/ex/SpringBootPsmIctExampleApplication.java
@@ -0,0 +1,13 @@
+package edu.ict.ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPsmIctExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPsmIctExampleApplication.class, args);
+	}
+
+}
