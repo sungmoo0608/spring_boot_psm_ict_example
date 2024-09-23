c5e919c49727cf6fd13949c5d64ca4c5abfaed0d
WIN-3QL0R8Q3DTD\ICT02_03 <swingsm0608@gmail.com>
2024-09-23 10:07:33 +0900

    EmpController_오기입_수정_2024_09_23

--- src/main/java/edu/ict/ex/controller/EmpController.java
+++ src/main/java/edu/ict/ex/controller/EmpController.java
@@ -36,16 +36,6 @@ public class EmpController {
 		return "emp/list";
 	}
 	
-	@GetMapping("/index")
-	public String index(Model model) {
-		
-		log.info("index()..");
-		
-		model.addAttribute("empList",empService.getEmpDeptSalgrade());
-		
-		return "emp/index";
-	}
-	
 	@GetMapping("/list7")
 	public String list7(Model model) {
 		
