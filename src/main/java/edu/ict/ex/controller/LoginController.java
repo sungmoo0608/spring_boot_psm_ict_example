6255b1131bbf74848ca299b591fc4c0e12fb6289
WIN-3QL0R8Q3DTD\ICT02_03 <swingsm0608@gmail.com>
2024-09-30 10:03:52 +0900

    세션 정보 로딩 방법 3가지

--- src/main/java/edu/ict/ex/controller/LoginController.java
+++ src/main/java/edu/ict/ex/controller/LoginController.java
@@ -1,5 +1,9 @@
 package edu.ict.ex.controller;
 
+import java.security.Principal;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +21,33 @@ public class LoginController {
 		log.info("login()..");
 
 		return "login/login";
+	}
+	
+	@GetMapping("/loginInfo")
+	public String loginInfo(Authentication authentication,Principal principal,Model model) {
+		
+		log.info("loginInfo()..");
+		
+		//1. 방법
+		if(principal != null) {
+			System.out.println(principal);
+			System.out.println(principal.getName());
+			System.out.println(principal);
+		}
+
+		//2. 방법
+		if(authentication != null) {
+			System.out.println(authentication);
+			System.out.println(authentication.getName());
+			System.out.println(authentication.getAuthorities());
+		}
+		
+		//3.SpringContextHolder를 통하여 가져오는 방법(일반적인 빈에서 사용할 수 있음)
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		System.out.println(auth.getName());
+		System.out.println(auth.getAuthorities());
+		
+		return "redirect:/";
 	}
 	
 }
